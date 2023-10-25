c47d9c5ed950d71cc7e8102aaaccb16729fa43d9
Toakan <7566941+Toakan@users.noreply.github.com>
2023-10-25 12:40:09 +0100

    Registered new functions
    Signed-off-by: Toakan <7566941+Toakan@users.noreply.github.com>

--- addons/sp_entities/cfgfunctions.hpp
+++ addons/sp_entities/cfgfunctions.hpp
@@ -2,19 +2,33 @@ class spe {
     //Simple Persist Entities
     tag = "spe";
 
-    class setup {
-        file = "sp_entities\functions\setup";
-        class mpnamespaceSet {};
-        class namespaceCheck {};
-        class namespaceGet {};
-        class namespaceUpdate {};
-        class profilenamespaceSet {};
-    };
-
     class sp_generalInit {
         file = "sp_entities\functions\init";
         class initSPE {
             postInit = 1;
         };
+    };
+
+    class setup {
+        file = "sp_entities\functions\setup";
+        class cleanup {};
+        class playerlookup {};
+        class unitsetup {};
+    };
+
+    class eh {
+        file = "sp_entities\functions\eh";
+        class missionEH {};
+    };
+
+    class actions {
+        file = "sp_entities\functions\actions";
+        class load {};
+        class save {};
+    };
+
+    class vehicle {
+        file = "sp_entities\functions\vehicle"
+        class getvehloadout {};
     };
 };
